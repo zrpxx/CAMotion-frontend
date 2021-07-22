98c0140da523892dc04ab0939d5e27714e9688f3
ylg <19301078@bjtu.edu.cn>
2021-07-22 15:55:18 +0800

    修改了Alert log:改变了表格内容，增加了筛选按钮，处理状态

--- src/pages/AlertLog.vue
+++ src/pages/AlertLog.vue
@@ -1,13 +1,35 @@
 <template>
+  <div>
   <q-card>
     <q-card-section>
       <div class="text-h6 text-grey-8">
-        Inline Actions
+        Alert Log
+        <!--
         <q-btn label="Export" class="float-right text-capitalize text-indigo-8 shadow-3" icon="person"/>
+        -->
+        <br>
+        <q-toggle
+          v-for="(option, index) in foptions"
+          :key="index"
+          v-model="option.value"
+          :label="option.label"
+          :color="option.color"
+          :keep-color="option.keepColor"
+          @input="toggleFilter(index)"
+        ></q-toggle>
+
+      </div>
+
+      <div>
+        <q-btn label="reset filter" color="primary" @click="resetFilter"></q-btn>
       </div>
     </q-card-section>
     <q-card-section class="q-pa-none">
-      <q-table :data="data" :columns="columns" hide-bottom>
+      <q-table :data="data"
+               :columns="columns" hide-bottom
+               :filter="filter"
+               :filter-method="myfilterMethod">
+
         <template v-slot:body-cell-Name="props">
           <q-td :props="props">
             <q-item style="max-width: 420px">
@@ -25,13 +47,19 @@
         </template>
         <template v-slot:body-cell-Action="props">
           <q-td :props="props">
-            <q-btn icon="edit" size="sm" flat dense/>
-            <q-btn icon="delete" size="sm" class="q-ml-sm" flat dense/>
+            <q-btn icon="image" size="sm" flat dense/>
+            <q-btn icon="check" size="sm" class="q-ml-sm" v-if="props.row.Result === 'unhandled'" @click = "props.row.Result = 'handled'" flat dense/>
+            <q-btn icon="cancel" size="sm" class="q-ml-sm" v-if="props.row.Result === 'handled'" @click = "props.row.Result = 'unhandled'" flat dense/>
           </q-td>
         </template>
       </q-table>
+
     </q-card-section>
+
   </q-card>
+
+  </div>
+
 </template>
 
 <script>
@@ -39,56 +67,105 @@ export default {
   name: "TableActions",
   data() {
     return {
+      foptions: [
+        {
+          label: 'unhandled',
+          value: false,
+          color: 'positive',
+          keepColor: true
+        },
+        {
+          label: 'handled',
+          value: false,
+          color: 'negative',
+          keepColor: true
+        }
+      ],
+
       data: [
         {
-          name: 'Pratik Patel',
-          Crated_Date: '15/3/2020',
-          Project: 'Quasar Admin',
-          avatar: 'https://avatars3.githubusercontent.com/u/34883558?s=400&u=09455019882ac53dc69b23df570629fd84d37dd1&v=4',
-          progress: 80,
-          des: 'Solutions Developer'
+          Time: '15/3/2021',
+          Camera_number: '#1',
+          Content: "zdd大傻子",
+          Result: "handled"
         },
         {
-          name: 'Mayank Patel',
-          Crated_Date: '10/2/2018',
-          Project: 'Quasar QDraggableTree',
-          avatar: 'https://avatars2.githubusercontent.com/u/27857088?s=400&u=a898efbc753d93cf4c2070a7cf3b05544b50deea&v=4',
-          progress: 50,
-          des: 'Solutions Developer'
+          Time: '15/4/2020',
+          Camera_number: '#1',
+          Content: "xxx",
+          Result: "unhandled"
         },
         {
-          name: 'Mayur Patel',
-          Crated_Date: '10/2/2018',
-          Project: 'Quasar Shopping',
-          avatar: 'https://avatars0.githubusercontent.com/u/55240045?s=400&u=cf9bffc2bd2d8e42ca6e5abf40ddd6c1a03ce2860&v=4',
-          progress: 100,
-          des: 'Solutions Developer'
+          Time: '15/3/2020',
+          Camera_number: '#1',
+          Content: "zdd大傻子",
+          Result: "handled"
         },
         {
-          name: 'Jeff Galbraith',
-          Crated_Date: '10/2/2019',
-          Project: 'Quasar QMarkdown',
-          avatar: 'https://avatars1.githubusercontent.com/u/10262924?s=400&u=9f601b344d597ed76581e3a6a10f3c149cb5f6dc&v=4',
-          progress: 60,
-          des: 'Solutions Developer'
+          Time: '15/3/2020',
+          Camera_number: '#1',
+          Content: "zdd大傻子",
+          Result: "unhandled"
         },
         {
-          name: 'Pratik Patel',
-          Crated_Date: '10/1/2020',
-          Project: 'Quasar QGrid',
-          avatar: 'https://avatars3.githubusercontent.com/u/34883558?s=400&u=09455019882ac53dc69b23df570629fd84d37dd1&v=4',
-          progress: 30,
-          des: 'Solutions Developer'
+          Time: '15/3/2020',
+          Camera_number: '#1',
+          Content: "zdd大傻子",
+          Result: "unhandled"
         },
       ],
       columns: [
-        {name: 'Name', label: 'Name', field: 'name', sortable: true, align: 'left'},
-        {name: 'Crated Date', label: 'Crated Date', field: 'Crated_Date', sortable: true, align: 'left'},
-        {name: 'Project', label: 'Project', field: 'Project', sortable: true, align: 'left'},
-        {name: 'Action', label: '', field: 'Action', sortable: false, align: 'center'}
+        {name: 'Time', label: 'Time', field: 'Time', sortable: true, align: 'center'},
+        {name: 'Camera_number', label: 'Camera_number', field: 'Camera_number', sortable: true, align: 'center'},
+        {name: 'Content', label: 'Content', field: 'Content', sortable: false, align: 'center'},
+        {name: 'Result', label: 'Result', field: 'Result', sortable: false, align: 'center'},
+        {name: 'Action', label: '操作', field: 'Action', sortable: false, align: 'center'}
       ],
+      filter: {value: 'none'},
     }
-  }
+  },
+
+    methods:
+    {
+      changeResult() {
+        this.data.Result = "handled";
+      },
+      toggleFilter(index)
+      {
+        if (index === 0 && this.foptions[0].value) {
+          this.foptions[1].value = false
+          this.filter.value = 'unhandled'
+          return
+        } else {
+          this.filter.value = 'none'
+        }
+
+        if (index === 1 && this.foptions[1].value) {
+          this.foptions[0].value = false
+          this.filter.value = 'handled'
+          return
+        } else {
+          this.filter.value = 'none'
+        }
+      },
+      resetFilter()
+      {
+        this.foptions.forEach(option => option.value = false)
+        this.filter.value = 'none'
+      },
+      myfilterMethod()
+      {
+        if (this.data.length > 2) {
+          if (this.filter.value === 'unhandled') {
+            return this.data.filter(row => (row.Result === 'unhandled'))
+          }
+          if (this.filter.value === 'handled') {
+            return this.data.filter(row => (row.Result === 'handled'))
+          }
+          return this.data
+        }
+      }
+    }
 }
 </script>
 
